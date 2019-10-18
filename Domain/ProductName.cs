24c959e39a6f26e0e0eb08e862789128169d9c3a
Sten Ehrberg <sten.ehrberg@gmail.com>
2019-10-18 21:55:29 +0300

    Initial commit

---
+++ Domain/ProductName.cs
@@ -0,0 +1,7 @@
+namespace Domain
+{
+    public class ProductName : Entity
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<ProductName> ProductNames { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ProductName>()
+                .HasData(
+                    new ProductName { Id = 1, Name = "Haamer" },
+                    new ProductName { Id = 2, Name = "Nuga" },
+                    new ProductName { Id = 3, Name = "Kaabel" }
+                );
+        }
+    }
+}
