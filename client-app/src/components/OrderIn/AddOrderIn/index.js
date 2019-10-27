d59558d74bff36388e4bd9d492c893ad140d46a2
Sten Ehrberg <sten.ehrberg@gmail.com>
2019-10-27 23:47:25 +0200

    grid changes on addOrderIn, vendor select update

--- client-app/src/components/OrderIn/AddOrderIn/index.js
+++ client-app/src/components/OrderIn/AddOrderIn/index.js
@@ -5,13 +5,14 @@ import { Grid, Button, Dropdown } from "semantic-ui-react";
 import AddProductName from "./AddProductName";
 import AddVendor from "./AddVendor";
 import { openVendorModal, getVendors, resetNewVendorId } from "../../../actions/vendorActions";
+import { getProductNames } from "../../../actions/productNameActions";
 
 function toDropdownSelect(list, valueKey, textKey, additionalTextKey) {
     return _.map(list, item => {
         return {
             key: item[valueKey],
             value: item[valueKey],
-            text: !!additionalTextKey ? `${item[textKey]} - ${item[additionalTextKey]}` : [item.textKey]
+            text: !!additionalTextKey ? `${item[textKey]} - ${item[additionalTextKey]}` : item[textKey]
         };
     });
 }
@@ -19,11 +20,13 @@ function toDropdownSelect(list, valueKey, textKey, additionalTextKey) {
 class AddOrderIn extends React.Component {
     state = {
         vendorId: null,
+        productNameId: null,
         products: []
     };
 
     componentDidMount() {
         this.props.getVendors();
+        this.props.getProductNames();
     }
 
     componentDidUpdate() {
@@ -49,29 +52,52 @@ class AddOrderIn extends React.Component {
             <section>
                 <h2>Sisseostu lisamine</h2>
                 <hr />
-                <Grid.Row>
-                    <Grid.Column width={5}>
-                        <Dropdown
-                            placeholder="vali ostukoht..."
-                            fluid
-                            search
-                            selection
-                            value={this.state.vendorId}
-                            options={this.props.vendors}
-                            onChange={this.vendorSelected}
-                        />
-                        <Button primary onClick={this.props.openVendorModal}>
-                            Lisa ostukoht
-                        </Button>
-                        <AddVendor />
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column width={5}>
-                        <Button primary>Lisa tootenimi</Button>
-                        <AddProductName />
-                    </Grid.Column>
-                </Grid.Row>
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column width={2}><h4>Vali ostukoht</h4></Grid.Column>
+                        <Grid.Column width={3}>
+                            <Dropdown
+                                placeholder="vali ostukoht..."
+                                search
+                                selection
+                                value={this.state.vendorId}
+                                options={this.props.vendors}
+                                onChange={this.vendorSelected}
+                            />
+                        </Grid.Column>
+                        <Grid.Column width={1}>
+                            <Button primary onClick={this.props.openVendorModal}>
+                                +
+                            </Button>
+                            <AddVendor />
+                        </Grid.Column>
+                        <Grid.Column width={4}></Grid.Column>
+                        <Grid.Column width={6}><h3>Sisseostu info</h3></Grid.Column>
+                    </Grid.Row>
+                    <h3>Toote lisamine</h3>
+                    <Grid.Row>
+                        <Grid.Column width={3}>
+                            <Dropdown
+                                placeholder="vali tootenimi..."
+                                search
+                                selection
+                                value={this.state.productNameId}
+                                options={this.props.productNames}
+                                onChange={this.vendorSelected}
+                            />
+                        </Grid.Column>
+                        <Grid.Column width={1}>
+                            <Button primary>+</Button>
+                                <AddProductName />
+                            </Grid.Column>
+                        <Grid.Column width={6} textAlign="right">Detailid</Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={10}>
+                            Toote tabel
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </section>
         );
     }
@@ -80,12 +106,12 @@ class AddOrderIn extends React.Component {
 const mapStateToProps = state => {
     return {
         vendors: toDropdownSelect(state.vendor.list, "id", "name", "address"),
-        productNames: toDropdownSelect(state.productName.list, "id", "name"),
+        productNames: toDropdownSelect(state.productName.list, "id", "name", null),
         newVendorId: state.vendor.newVendorId
     };
 };
 
 export default connect(
     mapStateToProps,
-    { openVendorModal, getVendors, resetNewVendorId }
+    { openVendorModal, getVendors, getProductNames, resetNewVendorId }
 )(AddOrderIn);
