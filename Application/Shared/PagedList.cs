e1d6e146f70ba5db0f35022b28a2aa90514a42a0
Sten Ehrberg <sten.ehrberg@gmail.com>
2020-02-03 21:23:14 +0200

    logic fix pageable list

--- Application/Shared/PagedList.cs
+++ Application/Shared/PagedList.cs
@@ -18,23 +18,24 @@ namespace Application.Shared
         public PagedList(IQueryable<DomainType> items, IPageableQuery pageableQuery, IMapper mapper)
         {
             TotalItems = items.Count();
+            PageNum = pageableQuery.PageNum ?? 1;
+            var domainItems = items.Skip((PageNum - 1) * PAGESIZE).Take(PAGESIZE).ToList();
+            SetProps(domainItems, pageableQuery, mapper);
+        }
+
+        public PagedList(IEnumerable<DomainType> items, IPageableQuery pageableQuery, IMapper mapper)
+        {
+            TotalItems = items.Count();
+            PageNum = pageableQuery.PageNum ?? 1;
             var domainItems = items.Skip((PageNum - 1) * PAGESIZE).Take(PAGESIZE).ToList();
             SetProps(domainItems, pageableQuery, mapper);
         }
 
         private void SetProps(IList<DomainType> domainItems, IPageableQuery pageableQuery, IMapper mapper)
         {
-            PageNum = pageableQuery.PageNum ?? 1;
             this.Items = mapper.Map<List<ItemType>>(domainItems);
             double totalPages = (double)TotalItems / PAGESIZE;
             TotalPages = (int)Math.Ceiling(totalPages);
-        }
-
-        public PagedList(IEnumerable<DomainType> items, IPageableQuery pageableQuery, IMapper mapper)
-        {
-            TotalItems = items.Count();
-            var domainItems = items.Skip((PageNum - 1) * PAGESIZE).Take(PAGESIZE).ToList();
-            SetProps(domainItems, pageableQuery, mapper);
         }
     }
 }
