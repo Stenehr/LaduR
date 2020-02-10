23f4085b29a28f09dd75ab54ed8183ae7046620b
Sten Ehrberg <sten.ehrberg@gmail.com>
2020-02-10 21:45:12 +0200

    working on datatable details

--- client-app/src/components/common/DataTable.tsx
+++ client-app/src/components/common/DataTable.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useState, Fragment } from 'react'
 import { Table, Icon } from 'semantic-ui-react';
 import { PagedList } from './types';
 
@@ -16,22 +16,24 @@ export interface IDataTableHeaderItem<T> {
 
 const DataTable: React.FC<IProps<any>> = ({ toggler, source, header }) => {
 
+    const [openedStateDetails, setOpenCloseDetails] = useState(new Array<number>());
+
     const renderTd = (item: any, headerItem: IDataTableHeaderItem<any>) =>
         <Table.Cell key={headerItem.field}>{headerItem.format ? headerItem.format(item) : item[headerItem.field]}</Table.Cell>
 
-    const openedDetails: number[] = [];
-
     const openCloseDetails = (item: any) => {
+        const openedDetails = [...openedStateDetails];
+
         const itemIndex = openedDetails.indexOf(item.id);
         if (itemIndex > -1) {
             openedDetails.splice(itemIndex, 1);
         } else {
             openedDetails.push(item.id);
         }
-        console.log(openedDetails);
+        setOpenCloseDetails([...openedDetails]);
     }
 
-    const isOpened = (item: any) => openedDetails.indexOf(item.id) > -1;
+    const isOpened = (item: any) => openedStateDetails.indexOf(item.id) > -1;
 
     return (
         <div>
@@ -46,15 +48,24 @@ const DataTable: React.FC<IProps<any>> = ({ toggler, source, header }) => {
                 </Table.Header>
                 <Table.Body>
                     {source.items.map((item, index) => (
-                        <Table.Row key={index}>
-                            {toggler && 
-                                <Table.Cell>
-                                    <Icon onClick={() => openCloseDetails(item)} link name={(isOpened(item) ? "minus square" : "plus square")} />
-                                </Table.Cell>}
-                            {header.map((headerItem, index) => (
-                                renderTd(item, headerItem)
-                            ))}
-                        </Table.Row>
+                        <Fragment key={index}>
+                            <Table.Row>
+                                {toggler && 
+                                    <Table.Cell>
+                                        <Icon onClick={() => openCloseDetails(item)} link name={(isOpened(item) ? "minus square" : "plus square")} />
+                                    </Table.Cell>}
+                                {header.map((headerItem, index) => (
+                                    renderTd(item, headerItem)
+                                ))}
+                            </Table.Row>
+                            {isOpened(item) && 
+                                <Table.Row>
+                                    <Table.Cell positive colSpan={toggler ? header.length + 1 : header.length}>
+                                    TEST
+                                    </Table.Cell>
+                                </Table.Row>
+                            }
+                        </Fragment>
                     ))}
                 </Table.Body>
             </Table>
